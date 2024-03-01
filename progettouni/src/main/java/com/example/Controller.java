edd73c2883e362162d091e5b834fe73fe0f54fd0
LolloCarne <lollocarnevali81@gmail.com>
2024-03-01 18:10:32 +0100

    cambionome Controller

--- progettouni/src/main/java/com/example/Controller.java
+++ progettouni/src/main/java/com/example/Controller.java
@@ -17,7 +17,7 @@ public class Controller {
     @FXML
     void onBtnClick1(ActionEvent event) throws IOException{
         
-        Parent root = FXMLLoader.load(getClass().getResource("schermataUtente.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("utenteScene.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
