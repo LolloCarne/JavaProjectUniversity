2e57c3afcf667f72b89afcf35633c0e05e8748c3
LolloCarne <lollocarnevali81@gmail.com>
2024-03-01 17:58:00 +0100

    Inserimento schermate scenebuilder e gitignore

--- progettouni/src/main/java/com/example/App.java
+++ progettouni/src/main/java/com/example/App.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -18,21 +21,39 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
         // Schermata principale con tre bottoni
-        MainScreen mainScreen = new MainScreen();
-        mainScreen.start(stage);
+          try {
+            Parent root = FXMLLoader.load(getClass().getResource("homeScene.fxml"));
+            primaryStage.setTitle("SPACCA");
+            primaryStage.setResizable(false);
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+
+            primaryStage.setOnCloseRequest(event -> {
+                event.consume();
+                esci(primaryStage);
+            });
+            //Image icon = new Image(getClass().getResource("").toExternalForm());
+            //primaryStage.getIcons().add(icon);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+        public void esci(Stage primaryStage){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Esci");
+        alert.setHeaderText("Stai per uscire dal gioco");
+        alert.setContentText("Sei sicuro di voler uscire?");
+
+        if(alert.showAndWait().get() == ButtonType.OK){
+            primaryStage.close();
+        }
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
 
     public static void main(String[] args) {
         launch();
---
+++ progettouni/src/main/java/com/example/Controller.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class Controller {
+
+    private Stage stage;
+    private Scene scene;
+    @FXML
+    void onBtnClick1(ActionEvent event) throws IOException{
+        
+        Parent root = FXMLLoader.load(getClass().getResource("schermataUtente.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void onBtnClick2(ActionEvent event) {
+
+    }
+
+}
+
