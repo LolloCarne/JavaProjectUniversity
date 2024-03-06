897887546c21d4f2ab90b49f5c26241bf999b8a3
LolloCarne <lollocarnevali81@gmail.com>
2024-03-06 15:59:47 +0100

    aggiunta Gson e creazione salvataggio partita

--- progettouni/src/main/java/com/Helper/Partita.java
+++ progettouni/src/main/java/com/Helper/Partita.java
@@ -1,8 +1,14 @@
 package com.Helper;
-
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.DTO.Utente;
 
 import javafx.application.Preloader.ErrorNotification;
@@ -13,7 +19,45 @@ public class Partita {
     public Partita(){
         this.codice= generateRandomString(6);
         Partecipanti = new ArrayList<>();
+        //creo json
+
     }
+
+    public Partita(String codice){
+
+        //carico json
+    }
+
+        private void salvaPartita() {
+        try {
+            String fileName = "tuo_file.json";
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            List<Partita> partite;
+
+            // Leggi il contenuto del file JSON esistente
+            if (Files.exists(Paths.get(fileName))) {
+                try (FileReader reader = new FileReader(fileName)) {
+                    partite = gson.fromJson(reader, List.class);
+                }
+            } else {
+                partite = new ArrayList<>();
+            }
+
+            // Aggiungi la nuova Partita alla lista
+            partite.add(this);
+
+            // Scrivi la lista aggiornata nel file JSON
+            try (FileWriter writer = new FileWriter(fileName)) {
+                gson.toJson(partite, writer);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     
     public void addPartecipante(Utente u) throws Error{
         if (this.Partecipanti.size()>=4){
@@ -27,6 +71,7 @@ public class Partita {
         return this.Partecipanti;
     }
 
+    
     public String getCode(){
         return this.codice;
     }
